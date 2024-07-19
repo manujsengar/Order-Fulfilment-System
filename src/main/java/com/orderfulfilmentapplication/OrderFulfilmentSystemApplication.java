8de5de8279fb749a186a5a09fbd187dafc37d8f5
Manuj Singh Sengar <91259399+manujsengar@users.noreply.github.com>
2024-07-19 18:23:12 +0530

    commenting scheduling
    commenting scheduling since its not being used

--- src/main/java/com/orderfulfilmentapplication/OrderFulfilmentSystemApplication.java
+++ src/main/java/com/orderfulfilmentapplication/OrderFulfilmentSystemApplication.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
-@EnableScheduling
+//@EnableScheduling
 @EnableAsync
 @EnableJpaRepositories
 @EnableRetry
